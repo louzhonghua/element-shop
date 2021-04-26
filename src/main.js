e19a2d641765f4fa2be0708c14c3bc8ff3b95052
louzhonghua <2662554936@qq.com>
2021-04-26 17:48:37 +0800

    添加了商品分类

--- src/main.js
+++ src/main.js
@@ -8,9 +8,11 @@ import 'element-ui/lib/theme-chalk/index.css';
 //导入字体icon
 import '@/assets/css/fonts/iconfont.css'
 import "./assets/css/common.less";
+import ZkTable from 'vue-table-with-tree-grid'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
+Vue.use(ZkTable)
 
 
 
